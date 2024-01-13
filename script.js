e91dc4b2728ead19b4e86609075856974a10f073
Rafael Parra <rafael12@gmx.com>
2024-01-13 14:54:57 -0600

    added blur when image is clicked, need to show image though.

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
  menu.classList.toggle("open");
  icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', start() )
+
+//  function start(){
+//document.getElementById("clickableImage").addEventListener('click', function() {
+//   const img = document.querySelector(".profilePic");
+
+//   // Toggle the clickedPic class on the image
+//   img.classList.toggle("clickedPic");
+
+//   // Toggle the body-blur and exclude-blur classes on the body and image
+//   if (!document.body.classList.contains("body-blur")) {
+//     document.body.classList.add("body-blur");
+//     img.classList.add("exclude-blur");
+//   } else {
+//     document.body.classList.remove("body-blur");
+//     img.classList.remove("exclude-blur");
+//   }
+// })}}
+;
