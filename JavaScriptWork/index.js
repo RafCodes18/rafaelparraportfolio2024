9f910218e4a797c5e13cf50794bf7080da85db2a
Rafael Parra <rafael12@gmx.com>
2024-01-31 13:21:23 -0600

    Javascript bad

--- JavaScriptWork/index.js
+++ JavaScriptWork/index.js
@@ -1,2 +1,18 @@
 var data = [1, 2, 3, 4, 5];
 
+var button = document.getElementById("btn-click");
+var selectList = document.getElementById("mySelect");
+var option = document.createElement("option");
+
+button.onclick = function()
+{
+ console.log("button clicked");
+
+ if(!selectList.hasChildNodes()){
+  for(var i = 0; i < data.length; i++){
+   option.text = data[i];
+   option.value = data[i];
+   selectList.appendChild(option);
+  }
+ }
+}
